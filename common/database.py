93cee665043a44cc4de965cece594294b792898c
Mutyala-Rupesh <76247351+Mutyala-Rupesh@users.noreply.github.com>
2021-02-09 11:38:19 +0530

    Add files via upload

---
+++ common/database.py
@@ -0,0 +1,23 @@
+import pymongo
+
+
+class Database(object):
+    URI = "mongodb+srv://rupesh:rupesh1122@cluster0.rurj9.mongodb.net/test"
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['fullstack']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
